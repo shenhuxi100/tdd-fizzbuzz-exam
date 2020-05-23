ea00b85beb1938715014dc55fedf88b41bc6cfe0
yongzhiliu <393073080@qq.com>
2020-05-23 13:36:43 +0800

    should_say_Fizz_when_reporting_given_number_is_not_multiple_by_3

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,18 @@
 package cn.xpbootcamp.fizzbuzz;
 
+import java.util.HashMap;
+
 public class FizzBuzz {
+    private static HashMap<Integer, String> numberMapper = new HashMap<>();
+
+    static {
+        numberMapper.put(3, "Fizz");
+    }
+
     public static String getReportedFigure(int number) {
+        if (number % 3 == 0)
+            return numberMapper.get(3);
+
         return String.valueOf(number);
     }
 }
--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -7,9 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    void should_say_self_number_when_reporting_given_number_is_not_multiple_by_3_5_7 () {
+    void should_say_self_number_when_reporting_given_number_is_not_multiple_by_3_5_7() {
         assertEquals("1", FizzBuzz.getReportedFigure(1));
         assertEquals("4", FizzBuzz.getReportedFigure(4));
     }
 
+    @Test
+    void should_say_Fizz_when_reporting_given_number_is_not_multiple_by_3() {
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(6));
+        assertEquals("Fizz", FizzBuzz.getReportedFigure(12));
+    }
 }
